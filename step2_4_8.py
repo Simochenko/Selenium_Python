f62e594024246f9f0fe30dcb4b0caabed9fd0c94
SSV <s.simochenko@softinway.com>
2021-03-02 17:40:45 +0200

    update

--- step2_4_8.py
+++ step2_4_8.py
@@ -1,8 +1,9 @@
 import math
 import time
-from selenium.webdriver.support import expected_conditions as EC
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
 
@@ -15,7 +16,7 @@ try:
     browser.get("http://suninjuly.github.io/explicit_wait2.html")
     button = WebDriverWait(browser, 12).until(
         EC.text_to_be_present_in_element((By.ID, "price"), "100")
-        )
+    )
     button = browser.find_element(By.CSS_SELECTOR, "#book")
     button.click()
     x_element = browser.find_element(By.CSS_SELECTOR, "#input_value")
